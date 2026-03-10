8dc1f6a987116ee485a60d8a84464cac36f6ece0
Morten Smith <morten1222@hotmail.com>
2026-03-10 09:20:17 +0100

    Updates to pages.

--- javascribt.js
+++ javascribt.js
@@ -1,10 +1,10 @@
-// Følg guide https://www.youtube.com/watch?v=LqpF8fkKOHU
+// Følg guide https://www.youtube.com/watch?v=LqpF8fkKOHU med hjælp fra w3 schools.
 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
